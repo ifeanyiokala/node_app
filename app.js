261c95974606cf7d6875e9c6ace40dc68057c9f6
ifeanyiokala <ifeanyi.okala.247238@unn.edu.ng>
2023-01-04 22:54:11 +0100

    modules

---
+++ app.js
@@ -0,0 +1,16 @@
+// CommonJS, every file 
+// Modules - Encapsultated Code (only share minimum)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative flavour')
+require('./7-mind-grenade')
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+// sayHi(data.per)
+
+
+// console.log(data)
